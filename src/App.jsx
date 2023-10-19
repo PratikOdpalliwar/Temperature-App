91b17e97eaa1befa4c9da5955528fc04bfcaa6a8
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2023-10-19 03:53:07 +0000

    CSS is  added to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import './App.css'
+import React, { useState } from "react"
 
-export default function App() {
+function App() {
+  // const [temp, setTemp] = useState(10);
+  // const [tempDisColor, setTempDiscolor] = useState("cold")
+
+  // const increaseTemp = () => {
+  //   setTemp(temp + 1)
+  // }
+  // const decreaseTemp = () => {
+  //   setTemp(temp + 1)
+  // }
+
   return (
-    <div>
+    <div className="App">
+      <div className="container">
+
+        <div className="temp-container">
+          <div className="temp-display">10°C</div>
+        </div>
+
+        <div className="btn-conntainer">
+          <button className="btn1" /*onChange={increaseTemp}*/>+</button>
+          <button className="btn2"/*onChange={decreaseTemp}*/>-</button></div>
+      </div>
+
 
     </div>
+
+
   )
 }
+
+export default App;
