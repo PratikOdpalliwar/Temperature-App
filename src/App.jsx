4eb0cebde75708f0370185107bfb8ef262060f33
Pratik_Odpalliwar <pratikodpalliwar777@.com>
2023-10-19 18:14:11 +0000

    logic is added to the App

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,42 @@ import './App.css'
 import React, { useState } from "react"
 
 function App() {
-  // const [temp, setTemp] = useState(10);
-  // const [tempDisColor, setTempDiscolor] = useState("cold")
+  const [temp, setTemp] = useState(10);
+  const [tempDisColor, setTempDiscolor] = useState("cold")
 
-  // const increaseTemp = () => {
-  //   setTemp(temp + 1)
-  // }
-  // const decreaseTemp = () => {
-  //   setTemp(temp + 1)
-  // }
+  const increaseTemp = () => {
+    if (temp === 30) return;
+    const newTemp = temp + 1;
+    setTemp(newTemp);
+
+    if (newTemp >= 15) {
+      setTempDiscolor("hot");
+    }
+  }
+
+
+  const decreaseTemp = () => {
+    if (temp === 0) return;
+
+    const newTemp1 = temp - 1;
+    setTemp(newTemp1);
+    if (newTemp1 < 16) {
+        setTempDiscolor("cold");
+    }
+  };
+
 
   return (
     <div className="App">
       <div className="container">
 
         <div className="temp-container">
-          <div className="temp-display">10°C</div>
+          <div className={`temp-display ${tempDisColor}`}>{temp}°C</div>
         </div>
 
         <div className="btn-conntainer">
-          <button className="btn1" /*onChange={increaseTemp}*/>+</button>
-          <button className="btn2"/*onChange={decreaseTemp}*/>-</button></div>
+          <button className="btn1" onClick={decreaseTemp}>-</button>
+          <button className="btn2" onClick={increaseTemp}>+</button></div>
       </div>
 
 
